a82aea3ca7b3796afdaa13aa096302a6858133fb
chintandd <chintand987@gmail.com>
2025-06-03 17:42:13 +0530

    favi icon added

--- hey.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-    document.body.style.display = "none";
-    document.body.offsetHeight; // trigger reflow
-    document.body.style.display = "grid"; // restore display
-  });
-  
